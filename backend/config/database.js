36cc38503d1cb7cc8b5c86480233b3a99077906c
Saad Hasan <saadh393@gmail.com>
2025-02-12 09:35:16 +0600

    Deploy

--- backend/config/database.js
+++ backend/config/database.js
@@ -11,11 +11,15 @@ module.exports = {
     logging: true
   },
   production: {
-    storage: config.dbFile,
-    dialect: "sqlite",
-    seederStorage: "sequelize",
-    logQueryParameters: true,
-    typeValidation: true,
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
     define: {
       schema: process.env.SCHEMA
     }
